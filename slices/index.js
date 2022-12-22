610f4726c0c7b52bb3c28e23eee6a4ff0a257aac
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-12-22 16:58:25 +0300

    Write code from lesson "Подключение Redux Toolkit к React"

---
+++ slices/index.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from './counterSlice.js';
+
+export default configureStore({
+  reducer: {
+    // counter – это свойство будет внутри объекта общего состояния: state.counter
+    counter: counterReducer,
+  },
+});
